6e34b9e48268f456dc035e4b31943bf2c485167e
Denis Bevilacqua <denisbevilacqua6@gmail.com>
2021-07-14 09:36:20 -0300

    Icon for navigation Drawer and Tab

--- routes/MyDrawer.js
+++ routes/MyDrawer.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import { createDrawerNavigator } from '@react-navigation/drawer'
-import { AboutStack } from './MyStack';
+import {createDrawerNavigator} from '@react-navigation/drawer'
+import {AboutStack} from './MyStack';
 import MyTab from './MyTab';
+import {AntDesign, Ionicons} from "@expo/vector-icons";
+
 const MyDrawer = () => {
     const Drawer = createDrawerNavigator()
     return (
         <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen name="Home" component={MyTab} />
-            <Drawer.Screen name="Done" component={MyTab} />
+            <Drawer.Screen name="Home" component={MyTab}
+                           options={{
+                               title: 'Home',
+                               drawerIcon: ({focused, size}) => (
+                                   <AntDesign name="home" size={16} color="black"/>
+                               ),
+                           }}/>
+            <Drawer.Screen name="Done" component={MyTab}
+                           options={{
+                               title: 'Home',
+                               drawerIcon: ({focused, size}) => (
+                                   <Ionicons name="checkmark-done" size={16} color="black" />
+                               ),
+                           }}/>
         </Drawer.Navigator>
     )
 }
--- routes/MyTab.js
+++ routes/MyTab.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { AboutStack, HomeStack } from './MyStack'
+import {AntDesign, Ionicons} from "@expo/vector-icons";
 const MyTab = () => {
     const Tab = createBottomTabNavigator()
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    if (route.name === 'Home') {
+                        return <AntDesign name="home" size={16} color="black"/>;
+
+
+                    } else if (route.name === 'Done') {
+                        return <Ionicons name="checkmark-done" size={16} color="black" />;
+                    }
+                },
+            })}
+        >
             <Tab.Screen name="Home" component={HomeStack} />
             <Tab.Screen name="Done" component={AboutStack} />
         </Tab.Navigator>
